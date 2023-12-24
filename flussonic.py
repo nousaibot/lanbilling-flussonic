26bc07e8e2f9667174e3244e802585ffb78e7e3a
nousabot <nousaibot@gmail.com>
2023-12-24 18:16:37 +0300

    Optimization and comments

--- flussonic.py
+++ flussonic.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import uuid
 import os
 import requests
-from datetime import datetime
 from zeep import Client, Settings
+
+logins=[]
+fs_logins={}
+
+#Result log file
+r_log = '/var/log/billing/flussonic.log'
+
+#---------------------------------------Lanbilling---------------------------------------
 
 #LB accounting data
 agent_id = '5'
@@ -13,16 +19,6 @@ lbuser = 'admin'
 lbpasswd = 'pass'
 lbapi = 'http://127.0.0.1:34012/?wsdl'
 
-#Flusssonic accounting data
-file = 'flussonic.log'
-sysargv_auth = {"login": "admin", "password": "pass"}
-url = 'https://127.0.0.1/vsaas/api/v2/'
-
-#Result log file
-r_log = '/var/log/billing/flussonic.log'
-
-logins=[]
-fs_logins={}
 
 #Lanbilling API
 settings = Settings(strict=False, xml_huge_tree=True)
@@ -32,11 +28,24 @@ flt = {'login': 'admin', 'pass': ''}
 response = client.service.Login(lbuser, lbpasswd)
 
 
-# Lanbilling Get an account by ID
+# Lanbilling Get accounts by ID
 flt = client.get_type('ns0:soapFilter')
 flt = flt(agentid=agent_id)
 response = client.service.getVgroups(flt)
 
+#Lanbilling Prepare a list of users and statuses
+for i in range(len(response)):
+   if response[i]['blocked'] == 0:
+      logins.append([response[i]['login'],True])
+   else:
+      logins.append([response[i]['login'],False])
+
+#---------------------------------------Flussonic---------------------------------------
+
+#Flussonic accounting data
+file = 'flussonic.log'
+sysargv_auth = {"login": "admin", "password": "pass"}
+url = 'https://127.0.0.1/vsaas/api/v2/'
 
 def get_session():
    r = requests.post(url + 'auth/login', json=sysargv_auth)
@@ -52,12 +61,6 @@ def get_session():
       os.remove(file)
       exit(1)
 
-for i in range(len(response)):
-   if response[i]['blocked'] == 0:
-      logins.append([response[i]['login'],True])
-   else:
-      logins.append([response[i]['login'],False])
-
 if os.path.isfile(file):
    file_auth = open(file, 'r')
    session_id = file_auth.read()
@@ -65,34 +68,29 @@ else:
    print('no file')
    session_id = get_session()
 
+#Flussonic Get accounts
 headers = {'x-vsaas-session': session_id, 'X-Page-Limit':'99'}
 r = requests.get(url + 'users', headers=headers)
 
+#Flussonic Prepare a list of users and statuses
 for el in r.json():
     read = {str(el["login"]):(str(el["id"]), bool(el["enabled"]))}
     fs_logins.update(read)
 
-if os.path.isfile(file):
-   file_auth = open(file, 'r')
-   session_id = file_auth.read()
-else:
-   print('no file')
-   session_id = get_session()
-
 for i in logins:
-
-  if fs_logins.get(i[0]) != None:
-     if fs_logins.get(i[0])[1] != i[1]:
-        with open(r_log, 'a') as file:
-           if i[1] == True:
-              file.write(i[0] + ' '  + 'Enable\n')
-           else:
-              file.write(i[0] + ' '  + 'Disable\n')
+   if fs_logins.get(i[0]) == None:
+      continue
+   elif fs_logins.get(i[0])[1] != i[1]:
+      with open(r_log, 'a') as file:
+         if i[1] == True:
+            file.write(i[0] + ' '  + 'Enable\n')
+         else:
+           file.write(i[0] + ' '  + 'Disable\n')
            #file.write(i[0] + ' ' + str(fs_logins.get(i[0])[1]) + ' ' +  str(i[1]) + '\n')
 
-        status = i[1]
-        payload = {"enabled":status}
-        user_id = 'users/{0}'.format(fs_logins.get(i[0])[0])
-        r = requests.put(url + user_id, data=payload, headers=headers)
-        if r.status_code == 403:
-           print('error 403')
+      status = i[1]
+      payload = {"enabled":status}
+      user_id = 'users/{0}'.format(fs_logins.get(i[0])[0])
+      r = requests.put(url + user_id, data=payload, headers=headers)
+      if r.status_code == 403:
+         print('error 403')
